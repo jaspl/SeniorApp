246713e97ae8e9ac1840b0e6d043bac5f1cf4ab1
Marta96 <marta.piotrowska9609@gmail.com>
2020-07-06 00:14:43 +0200

    Add next part's game of number game

--- app/src/main/java/com/example/seniorapp/Games/NumberGame/NumberGameActivity.java
+++ app/src/main/java/com/example/seniorapp/Games/NumberGame/NumberGameActivity.java
@@ -1,27 +1,42 @@
 package com.example.seniorapp.Games.NumberGame;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.seniorapp.GameSelectorActivity;
 import com.example.seniorapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static java.lang.Integer.*;
+
 public class NumberGameActivity extends AppCompatActivity {
     int digitsNumber = 0;
     int exercises = 0;
-    List<Integer> randomDigitsList; // lista cyfr
+    List<Integer> randomDigitsList = new ArrayList<>(); // lista cyfr
     int searchNumber = 0;   // do wyswietlenia jakiej liczby szukamy
     int count = 0;  // do wyswietlenia ile jest par do znalezienia
+    int click_two = 0;
+    int countNumberPairs = 0;
+    Button number_one = null;
+    Button number_two = null;
+    long startTime = System.currentTimeMillis();
+    long endTime = 0;
+    TextView countNumberPair = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +44,12 @@ public class NumberGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_number_game);
         setLvl();
         setEndGameButton();
+        setStartTime();
     }
 
     private int getLvl() {
         //TODO get lvl
-        int lvl = 5;
+        int lvl = 3;
         return lvl;
     }
 
@@ -63,23 +79,35 @@ public class NumberGameActivity extends AppCompatActivity {
         }
 
         Random rand = new Random();
-        int exercise = rand.nextInt(exercises);
+        int exercise = rand.nextInt(exercises+1);
+        TextView exercisesView = findViewById(R.id.exercisesView);
         if (exercise == 0) {
             generateRandomNumbersAdditions(digitsNumber);
+            exercisesView.append("Dodawanie");
         } else if( exercise == 1) {
             generateRandomNumbersMultiplications(digitsNumber);
+            exercisesView.append("Mnożenie");
         }
+        Log.d(String.valueOf(searchNumber), "search: ");
         setButtons(digitsNumber, getButtonsLayout());
+        TextView searchNumberView = findViewById(R.id.search_number);
+        searchNumberView.setText(String.valueOf(searchNumber));
+
+        countNumberPair = findViewById(R.id.count_number_pair);
+        countNumberPair.setText("0/" + String.valueOf(count));
     }
 
     private void setButtons(int digitsNumber, LinearLayout numberButtonsLayout) {
         for (int i = 0; i < digitsNumber; i++) {
             Button numberButton = new Button(this);
+            numberButton.setBackgroundColor(Color.LTGRAY);
             numberButton.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) numberButton.getLayoutParams();
             params.setMargins(0, 0, 10, 0);
             numberButton.setLayoutParams(params);
-            numberButton.setText(randomDigitsList.get(i));
+            int j = randomDigitsList.get(i);
+            numberButton.setText(String.valueOf(j));
+            numberButton.setTag(j);
             numberButtonsLayout.addView(numberButton);
             setNumberButtonClickAction(numberButton);
         }
@@ -87,12 +115,13 @@ public class NumberGameActivity extends AppCompatActivity {
 
     private void generateRandomNumbersAdditions(int digitsNumber) {
         Random rand = new Random(); //instance of random class
-        int upperbound = 9;
-        searchNumber = rand.nextInt(18);
+        searchNumber = rand.nextInt(19);
         do {
             //generate random values from 0-24
+            rand = new Random();
+            randomDigitsList.clear();
             for (int i = 0; i < digitsNumber; i++) {
-                randomDigitsList.add(rand.nextInt(upperbound));
+                randomDigitsList.add(rand.nextInt(10));
             }
             count = 0;
             for (int i = 1; i < digitsNumber; i++) {
@@ -105,12 +134,14 @@ public class NumberGameActivity extends AppCompatActivity {
 
     private void generateRandomNumbersMultiplications(int digitsNumber) {
         Random rand = new Random(); //instance of random class
-        int upperbound = 9;
         searchNumber = rand.nextInt(81);
         do {
             //generate random values from 0-24
+            rand = new Random();
+            randomDigitsList.clear();
             for (int i = 0; i < digitsNumber; i++) {
-                randomDigitsList.add(rand.nextInt(upperbound));
+                int r = rand.nextInt(9);
+                randomDigitsList.add(r);
             }
             count = 0;
             for (int i = 1; i < digitsNumber; i++) {
@@ -125,7 +156,32 @@ public class NumberGameActivity extends AppCompatActivity {
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Log.d(colorButton.getTag().toString(), "onClick:");
+                colorButton.setBackgroundColor(Color.BLUE);
+                click_two ++;
+                if (click_two == 1) {
+                    number_one = colorButton;
+                } else if (click_two == 2) {
+                    number_two = colorButton;
+                    if (parseInt(number_one.getText().toString())+parseInt(number_two.getText().toString())==searchNumber) {
+                        countNumberPairs++;
+                        number_one.setBackgroundColor(Color.GREEN);
+                        number_two.setBackgroundColor(Color.GREEN);
+                        countNumberPair.setText(countNumberPairs + "/" + count);
+                        click_two = 0;
+                        if (countNumberPairs == count) {
+                            setEndTime();
+                            float reactionTimeInSeconds = (float) (endTime - startTime) / 1000;
+                            sendResultToDatabase();
+                            Log.d("Koniec", "Koniec: ");
+                            getDialog();
+                        }
+                    } else {
+                        number_one.setBackgroundColor(Color.LTGRAY);
+                        number_two.setBackgroundColor(Color.LTGRAY);
+                        click_two = 0;
+                    }
+                }
             }
         });
     }
@@ -149,5 +205,43 @@ public class NumberGameActivity extends AppCompatActivity {
 
     private LinearLayout getButtonsLayout() {
         return findViewById(R.id.number_game_button_layout);
+    }
+
+    private void getDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Czy chcesz kontynuować grę ?")
+                .setCancelable(false)
+                .setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(NumberGameActivity.this, NumberGameActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(NumberGameActivity.this, GameSelectorActivity.class);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("?");
+        alert.show();
+    }
+
+    private long getTime() {
+        return System.currentTimeMillis();
+    }
+
+    private void setStartTime() {
+        startTime = getTime();
+    }
+
+    private void setEndTime() {
+        endTime = getTime();
+    }
+
+    private void sendResultToDatabase() {
+        //TODO
     }
 }
