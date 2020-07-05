43c8a297a240012d641161178f7074ff3b90201b
Jan <janekpro996@gmail.com>
2020-07-05 09:54:53 +0200

    patient login done patient info in progress

--- app/src/main/java/com/example/seniorapp/API/Api.java
+++ app/src/main/java/com/example/seniorapp/API/Api.java
@@ -10,12 +10,19 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 public interface Api {
 
     @GET("caregivers/get")
     Call<CaregiversObject> caregiverInfoGetter(@Query("login") String login, @Query("password") String password);
+
+    @GET("patients/login/get")
+    Call<PatientsObject> getPatient(@Query("login") String login, @Query("password") String password);
+
+    @GET("patients/get")
+    Call<PatientsObject> getPatientInfo(@Query("patient id") int id);
 
     @GET("patients/get/all")
     Call<List<PatientsObject>> getPatientList();
@@ -25,5 +32,9 @@ public interface Api {
 
     @POST("patients/add")
     Call<PatientsObject> addPatient(@Body PatientsObject patientsObject);
+
+    @PUT("patients/put/update")
+    Call<PatientsObject> updatePatientData( @Body PatientsObject patientsObject);
+
 
 }
--- app/src/main/java/com/example/seniorapp/PatientLogInActivity.java
+++ app/src/main/java/com/example/seniorapp/PatientLogInActivity.java
@@ -2,17 +2,27 @@ package com.example.seniorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.seniorapp.API.Api;
+import com.example.seniorapp.API.ApiClass;
 import com.example.seniorapp.Games.ColorGame.ColorGameActivity;
 import com.example.seniorapp.Games.MemoryGame.MemoryGameActivity;
 import com.example.seniorapp.Games.SymbolsGame.SymbolsGameActivity;
+import com.example.seniorapp.Models.CaregiversObject;
+import com.example.seniorapp.Models.PatientsObject;
 import com.google.android.material.textfield.TextInputLayout;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PatientLogInActivity extends AppCompatActivity {
 
@@ -28,9 +38,7 @@ public class PatientLogInActivity extends AppCompatActivity {
         patientLogInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO Patient Log in action
-                Intent intent = new Intent(PatientLogInActivity.this, GameSelectorActivity.class);
-                startActivity(intent);
+              logIn();
             }
         });
     }
@@ -47,5 +55,36 @@ public class PatientLogInActivity extends AppCompatActivity {
     private void setError(){
         TextInputLayout textInputLayout = findViewById(R.id.patient_log_in_password_error);
         textInputLayout.setError("Login lub hasło jest nieprawidłowe!");
+    }
+    private void logIn(){
+        Api api = new ApiClass().getApi();
+        Call<PatientsObject> call = api.getPatient(getLogin(),getPassword());
+        ProgressDialog progressDialog = new ProgressDialogClass().CustomCallBack(this,"wczytywanie");
+        progressDialog.show();
+        call.enqueue(new Callback<PatientsObject>() {
+            @Override
+            public void onResponse(Call<PatientsObject> call, Response<PatientsObject> response) {
+
+                if (!response.isSuccessful()) {
+                    Log.d("code:", "" + response.code());
+                    setError();
+                    progressDialog.dismiss();
+                }else {
+                    Intent intent = new Intent(PatientLogInActivity.this, GameSelectorActivity.class);
+                    startActivity(intent);
+                    PatientsObject patientsObject= response.body();
+                    new SharedPrefs(getApplicationContext()).saveId(patientsObject.getId());
+                    Log.d("login", new SharedPrefs(getApplicationContext()).getId()+"");
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PatientsObject> call, Throwable t) {
+                //TODO set error dialog
+                Log.d("caretaker logIn", "onFailure: "+t.getMessage());
+                progressDialog.dismiss();
+            }
+        });
     }
 }
